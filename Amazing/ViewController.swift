3db9cf04086a366cfe4cd9714bb4dd98a88340b3
sarababaei <sarababaei212@gmail.com>
2020-03-18 01:29:56 +0330

    Initial Commit

---
+++ Amazing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amazing
+//
+//  Created by Sara Babaei on 3/18/20.
+//  Copyright © 2020 Sara Babaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
